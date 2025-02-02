f672e97d4ef8bca7bfc44d65968f39bdb2fe995d
Vladislav <zvlad9923@gmail.com>
2025-02-02 21:54:33 +0200

    test

--- script.js
+++ script.js
@@ -10,14 +10,15 @@ document.addEventListener("DOMContentLoaded", () => {
         }, index * 300);
     });
     const targetLast = document.querySelector('#targetInLoadImg'); // id div Element
+    const textLanguage = document.querySelector('.textLanguage'); // id div Element
     const lazyLoad__Img = document.querySelector('#FrontendInginer'); // id img Element
 
-    addImgForLazyLoading(targetLast, './assets/img/FrontendInginer.jpg', lazyLoad__Img);
+    addImgForLazyLoading(targetLast, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIrJ0LPtx8bhmYCh0Gwm9wDFnN1nIJo39pxw&s', lazyLoad__Img);
 
 
 
 
-    visionElement(targetLast).then(res => {
+    visionElement(textLanguage).then(res => {
         if (res) {
             blockProgrammingWidth.forEach((section, index) => {
                 setTimeout(() => {
